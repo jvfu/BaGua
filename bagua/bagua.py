77fde66952149c944ff9bf1ddbfc0559f3a97cb7
Floyda <floyda@163.com>
2016-03-22 02:42:09 +0800

    #爻的图形

--- bagua/bagua.py
+++ bagua/bagua.py
@@ -10,38 +10,40 @@ def shake_yao():
 def shake_gua64():
     return [shake_yao() for x in xrange(6)]
 
-def get_symbol(index):
-    utest = ["---"] * 6
-    return utest
-
 def get_yao_info(index):
     def has_yao_index(line):
         for i, v in enumerate(YAO_INDEX):
-            if v[0] in line or v[1] in line:
-                return i
-        return -1
+            if v[YIN] in line: return i, YIN
+            if v[YANG] in line: return i, YANG
+                
+        return -1, None
 
     original = ''
-    rlist = [original] * 6
+    rlist = [[0, original] for x in range(6)]
     lines = get_info(index)
     if lines is None: return rlist
 
     for line in lines:
-        yao_index = has_yao_index(line)
-        if yao_index >= 0 and rlist[yao_index] is original:
-            rlist[yao_index] = line
+        yao_index, yao_type = has_yao_index(line)
+        if yao_index >= 0 and rlist[yao_index][1] is original:
+            rlist[yao_index][0] = yao_type
+            rlist[yao_index][1] = line
 
     return rlist
 
 def get_url(index):
     return "http://baike.fututa.com/a57{}/".format(37 + index)
 
+def get_symbol(ctype):
+    return YAO_SYMBOL[ctype]
+
 def show(index):
-    symbol = get_symbol(index)
-    info = get_yao_info(index)
+    info_list = get_yao_info(index)
     for x in range(6):
-        s = info[5 - x].replace('\n', '')
-        print symbol[x], s.decode('utf-8').encode('utf-8')
+        c = info_list[5 - x]
+        symbol = get_symbol(c[0])
+        info = c[1].decode('utf-8').encode('utf-8')
+        info = info.replace('\n', '')
+        print symbol, '\t' * 2, info
 
     print get_url(index)
-
