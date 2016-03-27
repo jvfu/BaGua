b19fd5f39d4f6cbba5f521b30eea810512617624
Floyda <floyda@163.com>
2016-03-28 03:26:03 +0800

    # setup

--- setup.py
+++ setup.py
@@ -1,25 +1,26 @@
-﻿from distutils.core import setup
-import os
+﻿#-*- encoding: UTF-8 -*-
+from setuptools import setup, find_packages
 
 setup(
     name         = 'BaGua',
     version      = '0.01',
     author       = 'Floyda',
     author_email = 'floyda@163.com',
-    license      = 'GPL3',
+    license      = 'MIT',
     description  = '八卦是中国文化的基本哲学概念',
+    long_description = '八卦是中国文化的基本哲学概念...',
+    keywords='bagua chinese',
+    classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
     url          = '',
-    packages     = [
-        # 'cheat',
-        # 'cheat.cheatsheets',
-        # 'cheat.test',
-    ],
+    packages=find_packages(),
     package_data = {
         # 'cheat.cheatsheets': [f for f in os.listdir('cheat/cheatsheets') if '.' not in f]
     },
-    scripts          = ['bin/bagua'],
+    include_package_data=True,
+    zip_safe=True,
     install_requires = [
         'docopt >= 0.6.1',
         'pygments >= 1.6.0',
-    ]
+    ],
+    scripts          = ['bin/cheat'],
 )
